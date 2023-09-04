f60fe1526f9cbe218a69ed91bd4a605bb732fdcb
RobinDev <contact@piedweb.com>
2023-09-04 17:51:50 +0200

    ...

--- packages/crawler/src/Url.php
+++ packages/crawler/src/Url.php
@@ -426,7 +426,7 @@ final class Url
 
     public function setTitle(string $title): void
     {
-        $this->title = substr($title, 0, 300);
+        $this->title = substr($title, 0, 230);
     }
 
     public function getUpdatedAt(): \DateTimeInterface
@@ -589,7 +589,7 @@ final class Url
 
     public function setMetaDescription(string $metaDescription): void
     {
-        $this->metaDescription = substr($metaDescription, 0, 300);
+        $this->metaDescription = substr($metaDescription, 0, 230);
     }
 
     public function getH1(): string
@@ -599,7 +599,7 @@ final class Url
 
     public function setH1(string $h1): void
     {
-        $this->h1 = substr($h1, 0, 300);
+        $this->h1 = substr($h1, 0, 230);
     }
 
     /**
