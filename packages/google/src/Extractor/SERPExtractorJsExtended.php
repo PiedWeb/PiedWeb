58c07df3b5133cb4e406ff46ff4f416f7a79776c
RobinDev <contact@piedweb.com>
2024-02-22 19:12:57 +0100

    fix pixelPos

--- packages/google/src/Extractor/SERPExtractorJsExtended.php
+++ packages/google/src/Extractor/SERPExtractorJsExtended.php
@@ -36,17 +36,19 @@ class SERPExtractorJsExtended extends SERPExtractor
             return $this->getPixelPosForWithoutCache($element);
         }
 
-        $cache = new FilesystemAdapter();
+        $pixelPos = $this->getPixelPosForWithoutCache($element);
 
-        /** @var int */
-        $pixelPos = $cache->get(
-            sha1($this->html.'-'.($element instanceof \DOMNode ? $element->getNodePath() : $element)),
-            function (ItemInterface $item) use ($element): int {
-                $item->expiresAfter(86400);
+        // $cache = new FilesystemAdapter();
 
-                return $this->getPixelPosForWithoutCache($element);
-            }
-        );
+        // /** @var int */
+        // $pixelPos = $cache->get(
+        //     sha1($this->html.'-'.($element instanceof \DOMNode ? $element->getNodePath() : $element)),
+        //     function (ItemInterface $item) use ($element): int {
+        //         $item->expiresAfter(86400);
+
+        //         return $this->getPixelPosForWithoutCache($element);
+        //     }
+        // );
 
         return $pixelPos;
     }
@@ -58,6 +60,7 @@ class SERPExtractorJsExtended extends SERPExtractor
         }
 
         $element = $this->getBrowserPage()->querySelectorXPath($element);
+
         if (isset($element[0]) && null !== $element[0]->boundingBox()) {
             $boundingBox = $element[0]->boundingBox();
             if (! \is_array($boundingBox)) {
@@ -68,11 +71,11 @@ class SERPExtractorJsExtended extends SERPExtractor
                 return 0;
             }
 
-            if (! \is_int($boundingBox['y'])) {
+            if (! \is_int($boundingBox['y']) && ! \is_float($boundingBox['y'])) {
                 return 0;
             }
 
-            return $boundingBox['y'];
+            return \intval($boundingBox['y']);
         }
 
         return 0;
