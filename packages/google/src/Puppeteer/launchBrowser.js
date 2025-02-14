f8b344d6159e2af6192e37840de87ae0b0ad2e33
RobinDev <contact@piedweb.com>
2025-02-14 12:15:42 +0100

    add windowSize in launchBrowser

--- packages/google/src/Puppeteer/launchBrowser.js
+++ packages/google/src/Puppeteer/launchBrowser.js
@@ -3,6 +3,8 @@
 PROXY_GATE=https://127.0.0.1:9876 \
 CHROME_BIN=/usr/bin/google-chrome \
 node packages/google/src/Puppeteer/launchBrowser.js 'fr' > ws.log 2>&1 &
+
+PUPPETEER_HEADLESS=false node vendor/piedweb/google/src/Puppeteer/launchBrowser.js 'fr' '1920,1080' &
  */
 
 const { executablePath, Browser } = require('puppeteer');
@@ -10,10 +12,17 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
+/**
+ * @returns {Browser}
+ */
 async function launchBrowser() {
+  const headless = process.env.PUPPETEER_HEADLESS === 'false' ? false : true;
+  const windowSize = process.argv[3] ? process.argv[3] : '';
+  const proxy = process.env.PROXY_GATE ? process.env.PROXY_GATE : '';
+
   /** @type {Browser} */
   let browser = await puppeteer.launch({
-    headless: process.env.PUPPETEER_HEADLESS ?? true,
+    headless: headless,
     executablePath: process.env.CHROME_BIN ?? '/usr/bin/google-chrome',
     args: [
       ...[
@@ -24,12 +33,15 @@ async function launchBrowser() {
         '--disable-setuid-sandbox',
         // --proxy-server=127.0.0.1:9876
       ],
-      ...(process.env.PROXY_GATE ? ['--proxy-server=' + process.env.PROXY_GATE] : []),
+      ...(proxy ? ['--proxy-server=' + proxy] : []),
+      ...(windowSize ? ['--window-size=' + windowSize] : []),
     ],
   });
 
   // Wait for the browser to launch and retrieve the WebSocket endpoint
   console.log(await browser.wsEndpoint());
+
+  return browser;
 }
 
 launchBrowser().then(() => process.stdin.resume());
